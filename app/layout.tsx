b322a729d06ec85e5aff1cdcc7b4c4daf0ce3a97
Anthony Tagorda <anthtago@gmail.com>
2024-09-17 17:53:38 +0800

    Transferred source code from old repo

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+// Font
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-sans",
+});
+
+// Head
+export const metadata: Metadata = {
+  title: "Butterfly",
+  description: "A psychotherapy platform",
+};
+
+// Root
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-light-300 font-sans antialiases",
+          fontSans.variable
+        )}
+      >
+        {/* <ThemeProvider attribute="class" defaultTheme="light">
+          {children}
+        </ThemeProvider> */}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        Home     
+      </section>
+    </div>
+  );
+}
