8caf1ab7ece153ae84786baf796de8fd6d6fb9f3
HitlerKumar <2226699@slu.edu.ph>
2024-09-18 20:13:20 +0800

    login register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,14 @@
 
 import React, { useState } from 'react';
 import { useLogin } from '@/app/login/hooks/useLogin';
-import LoginForm from '@/app/login/components/LoginForm'; // Import the LoginForm component
-import '@/app/login/styles/login.css'; // Import your styles
+import LoginForm from '@/app/login/components/LoginForm';
+import '@/app/login/styles/login.css';
 
 const LoginPage: React.FC = () => {
-    const { login, error } = useLogin(); // Hook for handling login logic
+    const { login, error } = useLogin();
     const [loading, setLoading] = useState<boolean>(false);
 
-    // Handle form submission in the LoginPage and pass it to LoginForm
+   
     const handleLogin = async (email: string, password: string) => {
         setLoading(true);
         await login(email, password);
@@ -20,7 +20,6 @@ const LoginPage: React.FC = () => {
         <div className="min-h-screen flex items-center justify-center bg-blue-500">
             <div className="flex bg-white rounded-lg shadow-lg max-w-4xl w-full overflow-hidden">
                 
-                {/* Left Side - Logo and Information */}
                 <div className="w-1/2 flex flex-col items-center justify-center bg-blue-100 p-8">
                     <div className="flex flex-col items-center">
                         <img
@@ -34,7 +33,6 @@ const LoginPage: React.FC = () => {
                     </h2>
                 </div>
 
-                {/* Right Side - Login Form */}
                 <div className="w-1/2 p-8">
                     <h2 className="text-center text-2xl font-semibold mb-6">Login</h2>
                     <LoginForm onLogin={handleLogin} error={error} loading={loading} />
