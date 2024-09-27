49d1bc00cdb1b6666665ca3ddad126f088740e5e
Anthony Tagorda <anthtago@gmail.com>
2024-09-27 23:34:22 +0800

    Updated tailwind config and globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,13 @@ const config: Config = {
   ],
   prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         background: "hsl(var(--background))",
