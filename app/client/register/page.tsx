65255be6db3de95c3ca1a8475b2041eb077a1353
Anthony Tagorda <anthtago@gmail.com>
2024-10-07 23:06:25 +0800

    Deleted my version of Register and Login, and removed Form components

--- app/client/register/page.tsx
+++ app/client/register/page.tsx
@@ -1,29 +1,9 @@
-import RegisterForm from "@/components/forms/RegisterForm";
-import { getUser } from "@/lib/actions/client.actions";
-import Image from "next/image";
+import React from 'react'
 
-const Register = async ({ params: { userId } }: SearchParamProps) => {
-  const user = await getUser(userId);
-
+const Register = () => {
   return (
-    <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex-1 flex-col">
-          <Image
-            src="/assets/icons/butterfly-logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="client"
-            className="mb-12 h-10 w-fit"
-          />
+    <div>Register</div>
+  )
+}
 
-          <RegisterForm user={user} />
-
-          <p className="copyright py-12">© 2024 Butterfly</p>
-        </div>
-      </section>
-    </div>
-  );
-};
-
-export default Register;
+export default Register
