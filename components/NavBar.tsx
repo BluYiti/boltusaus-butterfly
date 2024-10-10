a59f78127b1ad98e2c2899fcdfeeec517d09ccf3
Anthony Tagorda <anthtago@gmail.com>
2024-10-10 12:13:39 +0800

    updated NavBar components (chat and notifications)

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-between p-4">
+    <div className="bg-light-200 flex items-center justify-between p-4">
       {/* Search Bar */}
       <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image
@@ -12,21 +12,28 @@ const NavBar = () => {
           height={16}
           width={16}
         />
-        <input type="text" placeholder="Search" className="w-[200px] p-2 bg-transparent outline-none"></input>
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-[200px] p-2 bg-transparent outline-none"
+        ></input>
       </div>
       {/* Icons and User */}
       <div className="flex items-center gap-6 justify-end w-full">
-        <div className="bg-light-200 rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+        <div className="bg-light-200 rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
           <Image
             src="/assets/icons/chat.svg"
             alt="Chat"
             height={20}
             width={20}
           />
+          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-red-400 text-white rounded-full text-xs">
+            1
+          </div>
         </div>
         <div className="bg-light-200 rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
           <Image
-            src="/assets/icons/notifications.svg"
+            src="/assets/icons/notification.svg"
             alt="Notifications"
             height={20}
             width={20}
@@ -40,7 +47,7 @@ const NavBar = () => {
           <span className="text-[10px] text-gray-500 text-right">Client</span>
         </div>
         <Image
-          src="/assets/icons/butterfly-logo-round.svg"
+          src="/assets/icons/butterfly-black&blue.svg"
           alt="Profile"
           height={36}
           width={36}
