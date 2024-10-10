16de5d9327f19f4e4afe488f266ba8f8f8044dfc
Anthony Tagorda <anthtago@gmail.com>
2024-10-10 12:17:29 +0800

    updated imports of components of the Client dashboard

--- components/client/ClientPage.tsx
+++ components/client/ClientPage.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import MoodTrackerCard from "./MoodTrackerCard";
-import ClientCalendar from "./ClientCalendar";
-import Announcements from "./Announcements";
-import SetAppointmentCard from "./SetAppointmentCard";
+import MoodTrackerCard from "./dashboard/MoodTrackerCard";
+import ClientCalendar from "./dashboard/ClientCalendar";
+import Announcements from "./dashboard/Announcements";
+import SetAppointmentCard from "./dashboard/SetAppointmentCard";
+import StartPreAssessmentCard from "./dashboard/StartPreAssessmentCard"
+import ReadingResourcesCard from "./dashboard/ReadingResourcesCard";
 
 const ClientPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* MIDDLE */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {/* SET APPOINTMENT CARD */}
+        <StartPreAssessmentCard/>
+        {/* Set Appointment Card*/}
         <SetAppointmentCard />
-        {/* MOOD TRACKER CARD */}
+        {/* Mood Tracker Card */}
         <MoodTrackerCard />
+        {/* Reading Resources Card */}
+        <ReadingResourcesCard />
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <ClientCalendar />
         <Announcements />
+        <ClientCalendar />
       </div>
     </div>
   );
