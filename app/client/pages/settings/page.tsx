1ff5a9f4501a0752640680fb82152fb519177eca
Allana Joyce R. Gabaldon <2223936@slu.edu.ph>
2024-10-12 22:06:47 +0800

    settings (wala pa yung iba, di ko alam ano ilalagay)

--- app/client/pages/settings/page.tsx
+++ app/client/pages/settings/page.tsx
@@ -1,59 +1,264 @@
 "use client";  // Ensure this is a Client Component
 
-import React from "react";
-import { useRouter } from "next/navigation";  // Correct import for Next.js 13 App Router
+import React, { useState } from "react";
+import Layout from '@/components/Sidebar/Layout';
+import items from '@/client/data/Links';
 
-const SecurityPage: React.FC = () => {
-  const router = useRouter();
-
-  // Function to handle going back to the previous page
-  const handleBack = () => {
-    router.back();
-  };
+const Modal = ({ isOpen, onClose, title, children }) => {
+  if (!isOpen) return null;
 
   return (
-    <div className="text-black flex justify-start p-6 bg-white min-h-screen relative">
-      {/* Back button */}
-      <button
-        className="absolute top-4 left-4 bg-blue-800 text-white w-12 h-12 rounded-full flex items-center justify-center"
-        onClick={handleBack}
-      >
-        {/* Icon for back button (chevron left or arrow left) */}
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-8 h-8">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-        </svg>
-      </button>
-
-      <div className="w-full max-w-2xl mr-auto mt-20">  {/* Added mt-20 for more space at the top */}
-        <h1 className="text-4xl font-bold mb-10 text-left">Security</h1>  {/* Added mb-10 to separate title from content */}
-
-        <div className="space-y-6 text-left">
-          {/* Private Account */}
-          <div>
-            <p className="text-2xl font-semibold">Private account</p>
-            <p className="text-lg text-gray-500">You only have access to your account</p>
-          </div>
-
-          {/* Assessments Privacy */}
-          <div>
-            <p className="text-2xl font-semibold">All assessments are private</p>
-            <p className="text-lg text-gray-500">Only the Psychotherapist can view your pre-assessment and assessment scores</p>
-          </div>
-
-          {/* Recorded Sessions */}
-          <div>
-            <p className="text-2xl font-semibold">Recorded sessions are only accessed by the Psychotherapist</p>
-          </div>
-
-          {/* Payment Accounts */}
-          <div>
-            <p className="text-2xl font-semibold">Payment Accounts</p>
-            <p className="text-lg text-gray-500">Can’t be accessed by Psychotherapists and Associates</p>
-          </div>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white rounded-lg p-6 w-96">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-xl font-semibold">{title}</h2>
+          <button onClick={onClose} className="text-gray-500 hover:text-gray-700">X</button>
         </div>
+        {children}
       </div>
     </div>
   );
 };
 
-export default SecurityPage;
+const SettingsPage = () => {
+  const [activeSection, setActiveSection] = useState<string | null>(null);
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
+  const [isChangePasswordModalOpen, setIsChangePasswordModalOpen] = useState(false);
+
+  // State variables for profile fields and password fields
+  const [contactNumber, setContactNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  // Validation check for the password
+  const isPasswordValid = newPassword.match(
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/
+  ) && newPassword === confirmPassword;
+
+  const isProfileValid = contactNumber.trim() !== "" && address.trim() !== "";
+
+  const toggleSection = (section: string) => {
+    setActiveSection(activeSection === section ? null : section);
+  };
+
+  const handleEditProfileClick = () => {
+    setIsProfileModalOpen(true);
+  };
+
+  const handleChangePasswordClick = () => {
+    setIsChangePasswordModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsProfileModalOpen(false);
+    setIsChangePasswordModalOpen(false);
+  };
+
+  // Handler for saving profile changes
+  const handleSaveProfile = () => {
+    // Placeholder function for saving to the database
+    console.log("Profile saved:", { contactNumber, address });
+    handleModalClose();
+  };
+
+  // Handler for saving password changes
+  const handleUpdatePassword = () => {
+    // Placeholder function for saving the new password to the database
+    console.log("Password updated:", newPassword);
+    handleModalClose();
+  };
+
+  return (
+    <Layout sidebarTitle="Butterfly" sidebarItems={items}>
+      <div className="flex-grow p-8 bg-gray-100">
+        {/* Settings Header */}
+        <div className="bg-white shadow-md rounded-lg p-6 mb-8 text-left">
+          <h2 className="text-3xl font-bold text-blue-800">Settings</h2>
+          <p className="text-gray-600 mt-2">Manage your account, privacy, and application settings below.</p>
+        </div>
+
+        {/* Account Section */}
+        <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+          <h3
+            className="text-xl font-semibold text-gray-700 cursor-pointer flex justify-between items-center"
+            onClick={() => toggleSection("account")}
+          >
+            Account
+            <button className="text-sm text-blue-400 underline">Edit</button>
+          </h3>
+          {activeSection === "account" && (
+            <div className="mt-4 text-gray-600 space-y-4">
+              <div className="flex justify-between items-center">
+                <p>Update your profile information:</p>
+                <button 
+                  className="px-4 py-2 bg-blue-400 text-white rounded hover:bg-blue-500"
+                  onClick={handleEditProfileClick} // Open the profile editing modal
+                >
+                  Edit Profile
+                </button>
+              </div>
+              <div className="flex justify-between items-center">
+                <p>Change your password:</p>
+                <button 
+                  className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                  onClick={handleChangePasswordClick} // Open the change password modal
+                >
+                  Change Password
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        
+
+        {/* Privacy and Security Section */}
+        <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+          <h3
+            className="text-xl font-semibold text-gray-700 cursor-pointer flex justify-between items-center"
+            onClick={() => toggleSection("privacy")}
+          >
+            Privacy and Security
+            <button className="text-sm text-blue-400 underline">Manage</button>
+          </h3>
+          {activeSection === "privacy" && (
+            <div className="mt-4 text-gray-600">
+              <div className="flex justify-between items-center">
+                <p>Manage your data-sharing preferences:</p>
+                <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+                  Manage Privacy
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Help and Support Section */}
+        <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+          <h3
+            className="text-xl font-semibold text-gray-700 cursor-pointer flex justify-between items-center"
+            onClick={() => toggleSection("help")}
+          >
+            Help and Support
+            <button className="text-sm text-blue-400 underline">Get Help</button>
+          </h3>
+          {activeSection === "help" && (
+            <div className="mt-4 text-gray-600 space-y-4">
+              <div className="flex justify-between items-center">
+                <p>Contact our support team:</p>
+                <button className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">
+                  Contact Support
+                </button>
+              </div>
+              <div className="flex justify-between items-center">
+                <p>Browse FAQs and guides:</p>
+                <button className="px-4 py-2 bg-blue-400 text-white rounded hover:bg-blue-500">
+                  View FAQs
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* About Section */}
+        <div className="bg-white shadow-md rounded-lg p-6">
+          <h3
+            className="text-xl font-semibold text-gray-700 cursor-pointer flex justify-between items-center"
+            onClick={() => toggleSection("about")}
+          >
+            About
+            <button className="text-sm text-blue-400 underline">Learn More</button>
+          </h3>
+          {activeSection === "about" && (
+            <div className="mt-4 text-gray-600">
+              <p>Learn more about the app and its features:</p>
+              <ul className="list-disc list-inside mt-2">
+                <li>Version: 1.0.0</li>
+                <li>Developed by: Boltusaurs</li>
+                <li>Website: <a href="#" className="text-blue-400 hover:underline">www.amperalta.com</a></li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+
+       {/* Modal for Editing Profile */}
+       <Modal isOpen={isProfileModalOpen} onClose={handleModalClose} title="Edit Profile">
+          <div className="mb-4">
+            <label className="block text-gray-700">Contact Number:</label>
+            <input
+              type="text"
+              className="w-full px-3 py-2 border rounded"
+              value={contactNumber}
+              onChange={(e) => setContactNumber(e.target.value)}
+              placeholder="Enter your contact number"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Address:</label>
+            <input
+              type="text"
+              className="w-full px-3 py-2 border rounded"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              placeholder="Enter your address"
+            />
+          </div>
+          <button
+            onClick={handleSaveProfile}
+            className={`px-4 py-2 text-white rounded ${isProfileValid ? 'bg-blue-500 hover:bg-blue-600' : 'bg-gray-400 cursor-not-allowed'}`}
+            disabled={!isProfileValid}
+          >
+            Save Changes
+          </button>
+        </Modal>
+
+        {/* Modal for Changing Password */}
+        <Modal isOpen={isChangePasswordModalOpen} onClose={handleModalClose} title="Change Password">
+          <div className="mb-4">
+            <label className="block text-gray-700">Current Password:</label>
+            <input
+              type="password"
+              className="w-full px-3 py-2 border rounded"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              placeholder="Enter your current password"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">New Password:</label>
+            <input
+              type="password"
+              className="w-full px-3 py-2 border rounded"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              placeholder="Enter your new password"
+            />
+            <p className="text-xs text-red-500 mt-1">Password must be 8-12 characters, contain a capital letter, a number, and a symbol.</p>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Confirm New Password:</label>
+            <input
+              type="password"
+              className="w-full px-3 py-2 border rounded"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Confirm your new password"
+            />
+          </div>
+          <button
+            onClick={handleUpdatePassword}
+            className={`px-4 py-2 text-white rounded ${isPasswordValid ? 'bg-blue-500 hover:bg-blue-600' : 'bg-gray-400 cursor-not-allowed'}`}
+            disabled={!isPasswordValid}
+          >
+            Update Password
+          </button>
+        </Modal>
+      </div>
+    </Layout>
+  );
+};
+
+export default SettingsPage;
