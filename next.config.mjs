0c80b08989d36ac61692e9dd6544ddd91627b645
BluYiti <2221614@slu.edu.ph>
2024-11-14 22:43:29 +0800

    Fixed image error

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     env: {
       NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
     },
+    images: {
+      domains: ['cloud.appwrite.io'],  // Add this line to allow images from cloud.appwrite.io
+    },
   };
   
   export default nextConfig;
