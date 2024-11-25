ba502f3cdc1027e350c6396a4a14f1ab100fa593
BluYiti <2221614@slu.edu.ph>
2024-11-26 00:44:39 +0800

    Reverted next config for development

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,18 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
   },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cloud.appwrite.io',
-        pathname: '/v1/storage/buckets/Images/files/**', // Adjust the pathname as needed
+        pathname: '/v1/storage/buckets/Images/files/**', // You can adjust the pathname as needed
       },
     ],
   },
 };
 
-// Exporting with the bundle analyzer
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})(nextConfig);
+export default nextConfig;
