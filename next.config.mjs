0b27aed59536fac1186f5a5efda2260cf51d4dcf
BluYiti <2221614@slu.edu.ph>
2024-11-26 00:39:13 +0800

    Tried fixing for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
-/** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 const nextConfig = {
-    output: 'standalone',
-    env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'cloud.appwrite.io',
-          pathname: '/v1/storage/buckets/Images/files/**', // You can adjust the pathname as needed
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+        pathname: '/v1/storage/buckets/Images/files/**', // Adjust the pathname as needed
+      },
+    ],
+  },
+};
+
+// Exporting with the bundle analyzer
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig);
