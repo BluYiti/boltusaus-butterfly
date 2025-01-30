20a87bf5118946bfee1ea688a7a7949baaf95d1e
Miyakka <2224626@slu.edu.ph>
2025-01-30 10:41:23 +0800

    Refactor RegisterPage layout for improved responsiveness; adjust image visibility and container alignment for better UI presentation.

--- app/register/pages/minor/page.tsx
+++ app/register/pages/minor/page.tsx
@@ -7,26 +7,28 @@ import RegisterForm from '@/register/pages/components/RegisterForm';
 
 const RegisterPage: React.FC = () => {
     return (
-        <div className='overflow-hidden flex flex-col items-center min-h-screen'>
-            <Back />
-            <h1 className="fixed top-5 left-20 text-[#2081c3] text-2xl md:text-3xl font-bold">Butterfly</h1>
-            <Image 
-                src={"/images/minorfly.png"} 
-                alt='butterfly' 
-                width={500}
-                height={500}
-                priority // Set priority for LCP
-                className="fixed object-contain max-w-full h-auto 3xl:left-48 3xl:top-64 sm:left-8 sm:top-40" // Maintain aspect ratio
-            />
-            <div className="flex justify-end items-end">
-                <div className="ml-[35rem]">
-                    <RegisterForm 
-                        isAdult={false}
-                        error={null} 
+            <div className='overflow-hidden flex flex-col items-center min-h-screen'>
+                <Back />
+                <h1 className="fixed top-5 left-20 text-[#2081c3] text-2xl md:text-3xl font-bold">Butterfly</h1>
+                <div className="hidden sm:block">
+                    <Image 
+                        src={"/images/minorfly.png"} 
+                        alt='butterfly' 
+                        width={500}
+                        height={500}
+                        priority // Set priority for LCP
+                        className="fixed object-contain max-w-full h-auto 3xl:left-48 3xl:top-64 sm:left-8 sm:top-40 left-4 top-20" // Maintain aspect ratio
                     />
                 </div>
+                <div className="flex justify-end items-end w-full px-4 sm:px-0">
+                    <div className="ml-0 sm:ml-[35rem] w-full sm:w-auto">
+                        <RegisterForm 
+                            isAdult={false}
+                            error={null} 
+                        />
+                    </div>
+                </div>
             </div>
-        </div>
     );
 };
 
