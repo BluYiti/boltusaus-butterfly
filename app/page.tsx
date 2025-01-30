f7df4452c073140b73ed8c4831ce47a0756f392a
Miyakka <2224626@slu.edu.ph>
2025-01-30 09:20:47 +0800

    Refactor FAQ and Contacts Sections for improved layout and responsiveness; adjust element alignment and spacing for better UI presentation.

--- app/page.tsx
+++ app/page.tsx
@@ -243,87 +243,85 @@ const HomePage: React.FC = () => {
         <div className="max-w-4xl mx-auto">
           <h1 className="text-4xl md:text-6xl lg:text-8xl font-paintbrush mb-8 text-blue-800">Frequently Asked Questions</h1>
           <div className="space-y-4 md:space-y-8">
-            {faqItems.map((faq, index) => (
-              <div key={index} className="space-y-2">
-                <button
-                  onClick={() => toggleOpen(index)}
-                  className="flex justify-between items-center w-full text-left text-lg md:text-xl font-semibold"
-                >
-                  {faq.question}
-                  <span>{open === index ? '-' : '+'}</span>
-                </button>
-                <div
-                  className={`overflow-hidden transition-all duration-300 ease-in-out ${open === index ? 'max-h-40' : 'max-h-0'}`}
-                >
-                  <p className="text-base md:text-lg mt-2">{faq.answer}</p>
-                </div>
-              </div>
-            ))}
+        {faqItems.map((faq, index) => (
+          <div key={index} className="space-y-2">
+            <button
+          onClick={() => toggleOpen(index)}
+          className="flex justify-between items-center w-full text-left text-lg md:text-xl font-semibold"
+            >
+          {faq.question}
+          <span>{open === index ? '-' : '+'}</span>
+            </button>
+            <div
+          className={`overflow-hidden transition-all duration-300 ease-in-out ${open === index ? 'max-h-40' : 'max-h-0'}`}
+            >
+          <p className="text-base md:text-lg mt-2">{faq.answer}</p>
+            </div>
+          </div>
+        ))}
           </div>
         </div>
       </section>
-
+      
       {/* Contacts Section */}
       <section id="contacts" className="py-8 md:py-32 bg-[#c2dffd]">
-        <div className="max-w-4xl mx-auto text-center md:text-left">
+        <div className="max-w-4xl mx-auto text-center md:text-left px-4">
           <h1 className="text-4xl md:text-6xl lg:text-8xl font-paintbrush mb-8 text-blue-800">Get in Touch</h1>
           <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8">
-            <ContactInfo 
-              Icon={FaMapMarkerAlt} 
-              title="Visit Us" 
-              description="Our Clinic is located at Unit 303 Sam-son's Building, Lower Mabini St., Baguio City" 
-            />
-            <ContactInfo 
-              Icon={FaEnvelope} 
-              title="Email Us" 
-              description="Feel free to send us an email at amperaltapsychservices@gmail.com" 
-            />
-            <ContactInfo 
-              Icon={FaPhone} 
-              title="Call Us" 
-              description="You can reach us at +63 9266 696 242" 
-            />
+        <ContactInfo 
+          Icon={FaMapMarkerAlt} 
+          title="Visit Us" 
+          description="Our Clinic is located at Unit 303 Sam-son's Building, Lower Mabini St., Baguio City" 
+        />
+        <ContactInfo 
+          Icon={FaEnvelope} 
+          title="Email Us" 
+          description="Feel free to send us an email at amperaltapsychservices@gmail.com" 
+        />
+        <ContactInfo 
+          Icon={FaPhone} 
+          title="Call Us" 
+          description="You can reach us at +63 9266 696 242" 
+        />
           </div>
-          <div className="text-center my-10">
-            <a href="https://maps.app.goo.gl/yiV8BHgQP4zqpPta8" className="relative inline-block hover:transition-colors duration-300 group mr-4" target="_blank" rel="noopener noreferrer">
-              <div className="w-12 h-12 flex items-center justify-center bg-white rounded-full">
-                <FaMap className="text-blue-800 text-3xl" />
-              </div>
-              <span className="absolute inset-0 bg-blue-300 rounded-full opacity-0 transition-opacity duration-300 group-hover:opacity-50"></span>
-            </a>
-            <a href="https://www.facebook.com/amperaltapsychservices" className="relative inline-block hover:transition-colors duration-300 group" target="_blank" rel="noopener noreferrer">
-              <div className="w-12 h-12 flex items-center justify-center bg-white rounded-full">
-                <FaFacebookF className="text-blue-800 text-3xl" />
-              </div>
-              <span className="absolute inset-0 bg-blue-300 rounded-full opacity-0 transition-opacity duration-300 group-hover:opacity-50"></span>
-            </a>
+          <div className="flex justify-center space-x-4 my-10">
+        <a href="https://maps.app.goo.gl/yiV8BHgQP4zqpPta8" className="relative inline-block hover:transition-colors duration-300 group" target="_blank" rel="noopener noreferrer">
+          <div className="w-12 h-12 flex items-center justify-center bg-white rounded-full">
+            <FaMap className="text-blue-800 text-3xl" />
+          </div>
+          <span className="absolute inset-0 bg-blue-300 rounded-full opacity-0 transition-opacity duration-300 group-hover:opacity-50"></span>
+        </a>
+        <a href="https://www.facebook.com/amperaltapsychservices" className="relative inline-block hover:transition-colors duration-300 group" target="_blank" rel="noopener noreferrer">
+          <div className="w-12 h-12 flex items-center justify-center bg-white rounded-full">
+            <FaFacebookF className="text-blue-800 text-3xl" />
+          </div>
+          <span className="absolute inset-0 bg-blue-300 rounded-full opacity-0 transition-opacity duration-300 group-hover:opacity-50"></span>
+        </a>
           </div>
         </div>
-
-
-        <div className="text-left mt-4 ml-36 flex justify-between items-center">
+        <div className="text-center md:text-left mt-4 md:ml-36 flex flex-col md:flex-row justify-center md:justify-between items-center">
           <div>
-            <button
-                type="button"
-                onClick={() => {
-                    setIsModalOpen(true);
-                    setModalContentType('terms');
-                }}
-                className="text-blue-500 hover:underline ml-1"
-            >
-                Terms and Conditions
-            </button>
-            &nbsp;|&nbsp;
-            <button
-                type="button"
-                onClick={() => {
-                    setIsModalOpen(true);
-                    setModalContentType('privacy');
-                }}
-                className="text-blue-500 hover:underline ml-1"
-            >
-                Privacy Policy
-            </button>
+        <button
+            type="button"
+            onClick={() => {
+            setIsModalOpen(true);
+            setModalContentType('terms');
+            }}
+            className="text-blue-500 hover:underline ml-1"
+        >
+            Terms and Conditions
+        </button>
+        &nbsp;|&nbsp;
+        <button
+            type="button"
+            onClick={() => {
+            setIsModalOpen(true);
+            setModalContentType('privacy');
+            }}
+            className="text-blue-500 hover:underline ml-1"
+        >
+            Privacy Policy
+        </button>
           </div>
         </div>
       </section>
